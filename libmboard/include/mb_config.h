80d973da47e9de7ddeb177d8e4ec01f89c7b00a1
Chris Greenough <werkprinzip@gmail>
2020-07-27 12:01:24 +0100

    First set

---
+++ libmboard/include/mb_config.h
@@ -0,0 +1,114 @@
+/* include/mb_config.h.  Generated from mb_config.h.in by configure.  */
+/* include/mb_config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <argp.h> header file. */
+/* #undef HAVE_ARGP_H */
+
+/* Define if the compiler supports C99 Variadic Macros */
+#define HAVE_C99_VARIADIC_MACROS 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <float.h> header file. */
+#define HAVE_FLOAT_H 1
+
+/* Define if the C complier supports __func__ */
+#define HAVE_FUNC /**/
+
+/* Define if the compiler supports GNU-style Variadic Macros */
+#define HAVE_GNU_VARIADIC_MACROS 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `cunit' library (-lcunit). */
+#define HAVE_LIBCUNIT 1
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to 1 if you have the <limits.h> header file. */
+#define HAVE_LIMITS_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define if you have POSIX threads libraries and header files. */
+#define HAVE_PTHREAD 1
+
+/* Define to 1 if you have the <stddef.h> header file. */
+#define HAVE_STDDEF_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Options used to configure package */
+#define INFO_CONFIG_ARGS " '--with-mpi=/usr/lib/x86_64-linux-gnu/openmpi'"
+
+/* Date package was configure */
+#define INFO_CONFIG_DATE "Mon Jul 27 11:56:06 BST 2020"
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "libmboard"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "shawn.chin@_NOSPAM_stfc.ac.uk"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libmboard"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libmboard 0.3.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libmboard"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.3.1"
+
+/* Define to necessary symbol if this constant uses a non-standard name on
+   your system. */
+/* #undef PTHREAD_CREATE_JOINABLE */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.3.1"
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
